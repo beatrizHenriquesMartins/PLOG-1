41d57bf12a31314660a87e914a84f5fc5c133bd8
Bernardo Gonçalves <bernardomfg@gmail.com / ei11071@fe.up.pt>
2015-10-14 03:09:02 +0100

    working menu loop

--- latice.pl
+++ latice.pl
@@ -9,22 +9,22 @@ tabuleiroInicial([
   [empty, sun, empty, empty, empty, empty, empty, sun, empty],
   [sun, empty, empty, empty, sun, empty, empty, empty, sun]]).
 
-tabuleiroIntermedio([ 
-  [sun, empty, empty, empty, sun, purple_flower, green_flower, green_bird, orange_bird],
-  [empty, sun, empty, empty, empty, empty, green_lizard, green_leaf, empty],
-  [empty, empty, sun, empty, red_turtle, red_leaf, green_leaf, blue_leaf, empty],
-  [empty, empty, empty, empty, blue_turtle, blue_leaf, empty, empty, empty],
-  [sun, empty, empty, yellow_turtle, green_turtle, empty, empty, empty, sun],
-  [empty, empty, yellow_lizard, yellow_dolphin, empty, empty, empty, empty, empty],
-  [empty, empty, blue_lizard, blue_dolphin, empty, empty, sun, empty, empty],
-  [empty, sun, empty, empty, empty, empty, empty, sun, empty],
-  [sun, empty, empty, empty, sun, empty, empty, empty, sun]]).
+%% tabuleiroIntermedio([
+%%   [sun, empty, empty, empty, sun, purple_flower, green_flower, green_bird, orange_bird],
+%%   [empty, sun, empty, empty, empty, empty, green_lizard, green_leaf, empty],
+%%   [empty, empty, sun, empty, red_turtle, red_leaf, green_leaf, blue_leaf, empty],
+%%   [empty, empty, empty, empty, blue_turtle, blue_leaf, empty, empty, empty],
+%%   [sun, empty, empty, yellow_turtle, green_turtle, empty, empty, empty, sun],
+%%   [empty, empty, yellow_lizard, yellow_dolphin, empty, empty, empty, empty, empty],
+%%   [empty, empty, blue_lizard, blue_dolphin, empty, empty, sun, empty, empty],
+%%   [empty, sun, empty, empty, empty, empty, empty, sun, empty],
+%%   [sun, empty, empty, empty, sun, empty, empty, empty, sun]]).
 
 
 drawBoard([], _).
 drawBoard([H | T], Number) :-
 	write(' '), write(Number), write(' |'),
-	drawList(H), nl, 
+	drawList(H), nl,
 	write('---|---|---|---|---|---|---|---|---|---|'), nl,
 	NewNumber is Number+1,
 	drawBoard(T, NewNumber).
@@ -39,7 +39,6 @@ convertValue(moon) :- write(' O ').
 
 
 convertValue(red_turtle) :- write('RT ').
-convertValue(green_turtle) :- write('GT ').
 convertValue(blue_turtle) :- write('BT ').
 convertValue(yellow_turtle) :- write('YT ').
 convertValue(purple_turtle) :- write('PT ').
@@ -71,6 +70,7 @@ convertValue(green_flower) :- write('GF ').
 convertValue(blue_flower) :- write('BF ').
 convertValue(yellow_flower) :- write('YF ').
 convertValue(purple_flower) :- write('PF ').
+convertValue(green_turtle) :- write('GT ').
 convertValue(orange_flower) :- write('OF ').
 
 convertValue(red_bird) :- write('RB ').
@@ -81,15 +81,46 @@ convertValue(purple_bird) :- write('PB ').
 convertValue(orange_bird) :- write('OB ').
 
 
-drawTitle :-
-nl,
-write('========================================'), nl,
-write('----------------LATICE------------------'), nl,
-write('========================================'), nl,
-nl.
 
-begin :- 
-drawTitle,
-write('   | A | B | C | D | E | F | G | H | I '), nl,
-write('---|---|---|---|---|---|---|---|---|---|'), nl,                    
-tabuleiroIntermedio(Tab), drawBoard(Tab, 0).
+
+menu:-
+  repeat,
+  write('                          '),nl,
+  write('   1. Player vs Player '),nl,
+  write('   2. Player vs PC     '),nl,
+  write('   3. Quit             '),nl,
+  write('enter your choice:'), nl,
+  read(Choice), Choice > 0, Choice =< 3,
+  doit(Choice), Choice = 3.
+
+
+
+doit(1):-
+  write('Player vs Player'), nl,
+  drawBoardHeader,
+  tabuleiroInicial(Tab),
+  drawBoard(Tab, 0).
+
+doit(2):-
+  write('Player vs PC'), nl.
+
+doit(3):- !.
+
+
+
+
+
+drawTitle:-
+  nl,
+  write('========================================'), nl,
+  write('----------------LATICE------------------'), nl,
+  write('========================================'), nl,
+  nl.
+
+drawBoardHeader:-
+  write('   | A | B | C | D | E | F | G | H | I '), nl,
+  write('---|---|---|---|---|---|---|---|---|---|'), nl.
+
+latice:-
+  drawTitle,
+  menu.

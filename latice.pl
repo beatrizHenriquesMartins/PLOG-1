1c3781377821d8848544df3088ae6b83599bb3bd
FilipaBP <up201307852@fe.up.pt>
2015-11-01 05:24:35 +0000

    colocar peças no tabuleiro, ainda sem verificações

--- latice.pl
+++ latice.pl
@@ -1,13 +1,13 @@
-%% tabuleiroInicial([
-%% 	[sun, empty, empty, empty, sun, empty, empty, empty, sun],
-%%   [empty, sun, empty, empty, empty, empty, empty, sun, empty],
-%%   [empty, empty, sun, empty, empty, empty, sun, empty, empty],
-%%   [empty, empty, empty, empty, empty, empty, empty, empty, empty],
-%%   [sun, empty, empty, empty, moon, empty, empty, empty, sun],
-%%   [empty, empty, empty, empty, empty, empty, empty, empty, empty],
-%%   [empty, empty, sun, empty, empty, empty, sun, empty, empty],
-%%   [empty, sun, empty, empty, empty, empty, empty, sun, empty],
-%%   [sun, empty, empty, empty, sun, empty, empty, empty, sun]]).
+ tabuleiroInicial([
+	 [sun, empty, empty, empty, sun, empty, empty, empty, sun],
+   [empty, sun, empty, empty, empty, empty, empty, sun, empty],
+   [empty, empty, sun, empty, empty, empty, sun, empty, empty],
+   [empty, empty, empty, empty, empty, empty, empty, empty, empty],
+   [sun, empty, empty, empty, moon, empty, empty, empty, sun],
+   [empty, empty, empty, empty, empty, empty, empty, empty, empty],
+   [empty, empty, sun, empty, empty, empty, sun, empty, empty],
+   [empty, sun, empty, empty, empty, empty, empty, sun, empty],
+   [sun, empty, empty, empty, sun, empty, empty, empty, sun]]).
 
 tabuleiroIntermedio([
   [sun, empty, empty, empty, sun, purple-flower, green-flower, green-bird, orange-bird],
@@ -20,27 +20,76 @@ tabuleiroIntermedio([
   [empty, sun, empty, empty, empty, empty, empty, sun, empty],
   [sun, empty, empty, empty, sun, empty, empty, empty, sun]]).
 
-%tiles([red-turtle, blue-turtle, yellow-turtle, purple-turtle, orange-turtle, red-leaf]).
+tiles([red-turtle, green-turtle, blue-turtle, yellow-turtle, purple-turtle, orange-turtle, 
+       red-leaf, green-leaf, blue-leaf, yellow-leaf, purple-leaf, orange-leaf, 
+       red-dolphin, green-dolphin, blue-dolphin, yellow-dolphin, purple-dolphin, orange-dolphin,
+       red-lizard, green-lizard, blue-lizard, yellow-lizard, purple-lizard, orange-lizard,
+       red-flower, green-flower, blue-flower, yellow-flower, purple-flower, orange-flower,
+       red-bird, green-bird, blue-bird, yellow-bird, purple-bird, orange-bird]).
 
 
 
+%%------------------------------%%
+%%     POUCA JORDA NA MATRIZ    %%
+%%------------------------------%%
 
-drawBoard([], _).
-drawBoard([H | T], Number) :-
-	write(' '), write(Number), write(' |'),
-	drawList(H), nl,
-	write('---|---|---|---|---|---|---|---|---|---|'), nl,
-	NewNumber is Number+1,
-	drawBoard(T, NewNumber).
+%%procurar posicao v
 
-drawList([]).
-drawList([H | T]) :-
-	convertValue(H), write('|'), drawList(T).
+getMatrixElemAt(0, ElemCol, [ListAtTheHead|_], Elem):-
+  getListElemAt(ElemCol, ListAtTheHead, Elem).
+getMatrixElemAt(ElemRow, ElemCol, [_|RemainingLists], Elem):-
+  ElemRow > 0,
+  ElemRow1 is ElemRow-1,
+  getMatrixElemAt(ElemRow1, ElemCol, RemainingLists, Elem).
 
+
+getListElemAt(0, [ElemAtTheHead|_], ElemAtTheHead).
+getListElemAt(Pos, [_|RemainingElems], Elem):-
+  Pos > 0,
+  Pos1 is Pos-1,
+  getListElemAt(Pos1, RemainingElems, Elem).
+
+%%procurar posicao ^
+
+%%% 1. element row; 2. element column; 3. element to use on replacement; 3. current matrix; 4. resultant matrix.
+setMatrixElemAtWith(0, ElemCol, NewElem, [RowAtTheHead|RemainingRows], [NewRowAtTheHead|RemainingRows]):-
+  setListElemAtWith(ElemCol, NewElem, RowAtTheHead, NewRowAtTheHead).
+setMatrixElemAtWith(ElemRow, ElemCol, NewElem, [RowAtTheHead|RemainingRows], [RowAtTheHead|ResultRemainingRows]):-
+  ElemRow > 0,
+  ElemRow1 is ElemRow-1,
+  setMatrixElemAtWith(ElemRow1, ElemCol, NewElem, RemainingRows, ResultRemainingRows).
+
+%%% 1. position; 2. element to use on replacement; 3. current list; 4. resultant list.
+setListElemAtWith(0, Elem, [_|L], [Elem|L]).
+setListElemAtWith(I, Elem, [H|L], [H|ResL]):-
+  I > 0,
+  I1 is I-1,
+  setListElemAtWith(I1, Elem, L, ResL).
+
+%%% 1. element to be replaced; 2. element to use on replacements; 3. current matrix; 4. resultant matrix.
+replaceMatrixElemWith(_, _, [], []).
+replaceMatrixElemWith(A, B, [Line|RL], [ResLine|ResRL]):-
+  replaceListElemWith(A, B, Line, ResLine),
+  replaceMatrixElemWith(A, B, RL, ResRL).
+
+%%% 1. element to be replaced; 2. element to use on replacements; 3. current list; 4. resultant list.
+replaceListElemWith(_, _, [], []).
+replaceListElemWith(A, B, [A|L1], [B|L2]):-
+  replaceListElemWith(A, B, L1, L2).
+replaceListElemWith(A, B, [C|L1], [C|L2]):-
+  C \= A,
+  replaceListElemWith(A, B, L1, L2).
+
+
+%%-------------------------------%%
+%% -/- POUCA JORDA NA MATRIZ -/- %%
+%%-------------------------------%%
+
+
+%% conver values v
 convertValue(sun) :- write(' * ').
 convertValue(empty) :- write('   ').
 convertValue(moon) :- write(' O ').
-
 
 convertValue(red-turtle) :- write('R +').
 convertValue(green-turtle) :- write('G +').
@@ -85,6 +134,13 @@ convertValue(yellow-bird) :- write('Y #').
 convertValue(purple-bird) :- write('P #').
 convertValue(orange-bird) :- write('O #').
 
+%%convertValues ^
+
+clearScreen(0).
+clearScreen(N):-
+  nl,
+  N1 is N-1,
+  clearScreen(N1).
 
 
 
@@ -95,24 +151,37 @@ menu:-
   write('   2. Player vs PC     '),nl,
   write('   3. Quit             '),nl,
   write('enter your choice:'), nl,
-  read(Choice), write('atao2'), nl, Choice > 0, Choice =< 3,
-  doit(Choice), Choice = 3.
+  read(Choice), nl, Choice > 0, Choice =< 3,
+  doit(Choice).
 
 
 
 doit(1):-
-  write('Player vs Player'), nl,
-  drawBoardHeader,
   tabuleiroIntermedio(Tab),
-  drawBoard(Tab, 0), !,
-  write('atao'), nl.
-
+  pvpgamimg(Tab).
 
 doit(2):-
+  clearScreen(40),
   write('Player vs PC'), nl.
 
 doit(3):- !.
 
+pvpgamimg(INITIALBOARD):-
+  clearScreen(40),
+  write('Player vs Player'), nl, nl,
+  drawTab(INITIALBOARD, 0), !,
+  nl, nl,
+  write('Piece to be Played:'), read(Piece), nl,
+  write('Row:'), read(Row), nl,
+  write('Column:'), read(Column), nl,
+
+  %%% 1. element row; 2. element column; 3. element to use on replacement; 3. current matrix; 4. resultant matrix.
+  
+  setMatrixElemAtWith(Row, Column, Piece, INITIALBOARD, Result),
+
+  pvpgamimg(Result).
+
+  
 
 
 
@@ -128,6 +197,40 @@ drawBoardHeader:-
   write('   | A | B | C | D | E | F | G | H | I '), nl,
   write('---|---|---|---|---|---|---|---|---|---|'), nl.
 
+
+
+drawTab([H | T], Number):-
+ drawBoardHeader,
+ drawBoard([H | T], Number).
+
+
+%%desenhar tabuleiro v
+drawBoard([], _).
+drawBoard([H | T], Number) :-
+  write(' '), write(Number), write(' |'),
+  drawList(H), nl,
+  write('---|---|---|---|---|---|---|---|---|---|'), nl,
+  NewNumber is Number+1,
+  drawBoard(T, NewNumber).
+
+drawList([]).
+drawList([H | T]) :-
+  convertValue(H), write('|'), drawList(T).
+
+%%desenhar tabuleiro ^
+
 latice:-
   drawTitle,
   menu.
+
+%%desenhar tabuleiro
+%%decidir jogador
+%%ler inputs
+%%verificar inputs
+%%se ok->atualizar e imprimir tabuleiro
+%%se ko->pedir nova jogada
+%%terminar ciclo na condiçao de vitoria
+
+
+
+

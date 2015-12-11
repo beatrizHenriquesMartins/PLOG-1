13067e4e7b0072238c8ff8a21c619e3f19b768cd
Bernardo Gonçalves <bernardomfg@gmail.com / ei11071@fe.up.pt>
2015-12-11 05:44:24 +0000

    pastas organizadas, base de conhecimento criada

---
+++ Proj 2/db.pl
@@ -0,0 +1,74 @@
+% Base de conhecimento %
+
+%Sopas - sopa(nome, [ingredientes])
+
+sopa(sopa_batata, [batata, cebola, alho]).
+sopa(legumes, [couve, cenoura, couve_branca, abobora, vagens, alho]).
+sopa(leguminosa_feijao, [feijao, batata, couve, cenoura, cebola]).
+sopa(leguminosa_favas, [favas, cebola, alho, espinafres]).
+
+
+carne(strogonoff, [peru, natas, cogumelos, esparguete]).
+carne(estufada, [porco, arroz, batata]).
+carne(bife_peru, [peru, arroz].
+carne(frango_assado, [frango, batata, arroz]).
+
+peixe(peixe_abrotea, [abrotea, batata]).
+peixe(bacalhau_pure, [bacalhau, batata, leite]).
+peixe(filetes, [pescada, batata, ovos]).
+
+salada(alface).
+salada(cebola).
+salada(cenoura).
+salada(pepin).
+salada(tomate).
+
+fruta(banan).
+fruta(laranja).
+fruta(maca).
+fruta(melancia).
+fruta(pera).
+
+cozida(banan).
+cozida(maca).
+cozida(pera).
+
+doce(aletria).
+doce(arroz_doce).
+doce(gelado).
+doce(gelatina).
+doce(iogurte).
+doce(leite_creme).
+doce(pudim).
+
+
+ingrediente(abobora).
+ingrediente(abrotea).
+ingrediente(alho).
+ingrediente(arroz).
+ingrediente(bacalhau).
+ingrediente(banan).
+ingrediente(batata).
+ingrediente(cebola).
+ingrediente(cenoura).
+ingrediente(cogumelos).
+ingrediente(couve).
+ingrediente(couve_branca).
+ingrediente(esparguete).
+ingrediente(espinafres).
+ingrediente(favas).
+ingrediente(feijao).
+ingrediente(frango).
+ingrediente(laranja).
+ingrediente(leite).
+ingrediente(maca).
+ingrediente(melancia).
+ingrediente(natas).
+ingrediente(ovos).
+ingrediente(pepin).
+ingrediente(pera).
+ingrediente(peru).
+ingrediente(pescada).
+ingrediente(porco).
+ingrediente(tomate).
+ingrediente(vagens).
